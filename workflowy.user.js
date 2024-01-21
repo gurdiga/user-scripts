93daae945ff0efeb8091cd86a3b2cf7fe622ae39
Vlad GURDIGA <gurdiga@gmail.com>
2024-01-21 11:55:23 +0200

    VSCode: format on save

--- workflowy.user.js
+++ workflowy.user.js
@@ -1,21 +1,20 @@
-(function() {
-    'use strict';
+(function () {
+  "use strict";
 
-    const rev = '7c8d562';
+  const rev = "7c8d562";
 
-    GM_addElement('link', {
-        href: `https://cdn.jsdelivr.net/gh/gurdiga/user-styles@${rev}/workflowy.css`,
-        rel: 'stylesheet'
-    });
+  GM_addElement("link", {
+    href: `https://cdn.jsdelivr.net/gh/gurdiga/user-styles@${rev}/workflowy.css`,
+    rel: "stylesheet",
+  });
 
-    console.log('+++ User-style added');
+  console.log("+++ User-style added");
 
-    GM_addElement('meta', {
-        name: "theme-color",
-        media: "(prefers-color-scheme: dark)",
-        content: "#1f1f1e"
-    });
+  GM_addElement("meta", {
+    name: "theme-color",
+    media: "(prefers-color-scheme: dark)",
+    content: "#1f1f1e",
+  });
 
-    console.log('+++ Theme-color set');
-
+  console.log("+++ Theme-color set");
 })();
