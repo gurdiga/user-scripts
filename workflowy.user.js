302748fcefd5d1e5c086f2b9c85a260905b54e4b
Vlad GURDIGA <gurdiga@gmail.com>
2024-01-21 11:53:15 +0200

    workflowy.user.js

---
+++ workflowy.user.js
@@ -0,0 +1,21 @@
+(function() {
+    'use strict';
+
+    const rev = '7c8d562';
+
+    GM_addElement('link', {
+        href: `https://cdn.jsdelivr.net/gh/gurdiga/user-styles@${rev}/workflowy.css`,
+        rel: 'stylesheet'
+    });
+
+    console.log('+++ User-style added');
+
+    GM_addElement('meta', {
+        name: "theme-color",
+        media: "(prefers-color-scheme: dark)",
+        content: "#1f1f1e"
+    });
+
+    console.log('+++ Theme-color set');
+
+})();
