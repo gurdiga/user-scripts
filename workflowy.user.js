990235a88e5aaee368df99a15b7fef333725926f
Vlad GURDIGA <gurdiga@gmail.com>
2024-01-22 08:58:28 +0200

    Workflowy: improve theme-color sync

--- workflowy.user.js
+++ workflowy.user.js
@@ -3,18 +3,29 @@
 
   const rev = "7c8d562";
 
+  console.log("+++ User-style added BEGIN");
+
   GM_addElement("link", {
     href: `https://cdn.jsdelivr.net/gh/gurdiga/user-styles@${rev}/workflowy.css`,
     rel: "stylesheet",
   });
 
-  console.log("+++ User-style added");
+  console.log("+++ User-style added END");
 
-  GM_addElement("meta", {
-    name: "theme-color",
-    media: "(prefers-color-scheme: dark)",
-    content: "#1f1f1e",
-  });
+  console.log("+++ Theme-color set BEGIN");
 
-  console.log("+++ Theme-color set");
+  const headerBackgroundColor = getComputedStyle(document.querySelector("#app .header")).backgroundColor;
+  const metaThemeColor = document.querySelector('meta[name="theme-color"]');
+
+  if (metaThemeColor) {
+    metaThemeColor.setAttribute("content", headerBackgroundColor);
+  } else {
+    GM_addElement("meta", {
+      name: "theme-color",
+      media: "(prefers-color-scheme: dark)",
+      content: headerBackgroundColor,
+    });
+  }
+
+  console.log("+++ Theme-color set END");
 })();
