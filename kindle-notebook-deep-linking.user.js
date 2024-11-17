bb2f696f0a3f19d82e22d4bab82c2a1fd42809d9
Vlad GURDIGA <gurdiga@gmail.com>
2024-11-17 10:50:24 +0200

    kindle-notebook-deep-linking.user.js

--- kindle-notebook-deep-linking.user.js
+++ kindle-notebook-deep-linking.user.js
@@ -3,22 +3,35 @@
 
   console.log("+++ User-script BEGIN: Kindle Notebook Deep Linking");
 
-  window.addEventListener("load", () => {
-    console.log("load");
+  const asin = location.hash.slice(1);
 
-    const asin = location.hash.slice(1);
+  console.log("+++ User-script", { asin });
 
-    if (!asin) {
-      console.log("No ASIN");
-      return;
-    }
+  if (!asin) {
+    console.log("+++ User-script No ASIN found");
+    return;
+  } else {
+    console.log("+++ User-script", { asin });
+  }
+
+  const interval = setInterval(() => {
+    console.log("+++ User-script setInterval");
 
     const sidebarThumbnail = document.getElementById(asin).firstElementChild;
 
-    console.log({ asin, sidebarThumbnail });
+    if (!sidebarThumbnail) {
+      console.log(`+++ User-script element not yet found by ID: "${asin}"`);
+      return;
+    }
+
+    console.log("+++ User-script", { sidebarThumbnail });
 
     sidebarThumbnail.click();
-  });
+
+    console.log("+++ User-script clicked sidebarThumbnail");
+
+    clearInterval(interval);
+  }, 300);
 
   console.log("+++ User-script END");
 })();
