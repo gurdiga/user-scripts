44ace6f589691f7ef78a01ab32649f5691324a57
Vlad GURDIGA <gurdiga@gmail.com>
2026-03-14 15:09:09 +0200

    WorkFlowy: style revision bump

--- workflowy.user.js
+++ workflowy.user.js
@@ -12,7 +12,7 @@
 (function () {
   "use strict";
 
-  const styleRevision = "bf61f6e";
+  const styleRevision = "631c36a";
   const cssUrl = `https://cdn.jsdelivr.net/gh/gurdiga/user-styles@${styleRevision}/workflowy.css`;
 
   const isDesktop = !(navigator.maxTouchPoints > 0);
