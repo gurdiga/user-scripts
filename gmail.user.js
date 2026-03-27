931fb31597375067c36a04ffe8bc2fe531c967f0
Vlad GURDIGA <gurdiga@gmail.com>
2026-03-27 09:49:24 +0200

    Gmail: style revision bump

--- gmail.user.js
+++ gmail.user.js
@@ -1,12 +1,12 @@
 (function () {
   "use strict";
 
-  const rev = "93e2e9f";
+  const rev = "8330277";
 
   GM_addElement("link", {
     href: `https://cdn.jsdelivr.net/gh/gurdiga/user-styles@${rev}/gmail.css`,
     rel: "stylesheet",
   });
 
-  console.log("+++ User-style added");
+  console.log(`+++ User-style added ${rev}`);
 })();
