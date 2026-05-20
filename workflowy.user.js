149c2bc6891f0ebb880c3b0c9a2bcee6c56f9b0d
Vlad GURDIGA <gurdiga@gmail.com>
2026-05-20 08:34:11 +0300

    WorkFlowy: add @require self for local dev

--- workflowy.user.js
+++ workflowy.user.js
@@ -7,6 +7,7 @@
 // @match        https://workflowy.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=workflowy.com
 // @grant        GM.addStyle
+// @require      file:///Users/vlad/src/user-scripts/workflowy.user.js
 // ==/UserScript==
 
 (function () {
